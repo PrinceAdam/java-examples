3e46fcb73b0fd04fae0d644a3dae7827b6cfb2a0
Adam Duncan <Adam Duncan>
2017-01-28 19:43:19 -0800

    added 'map' example and test

--- src/main/java/LambdaExpressions.java
+++ src/main/java/LambdaExpressions.java
@@ -6,6 +6,7 @@ public class LambdaExpressions {
 
     public String filterExample(){
         String[] input = {"tiger", "cat", "TIGER", "Tiger", "leopard"};
+
         List<String> cats = Arrays.asList(input);
         String search = "tiger";
         String tigers = cats.stream()
@@ -14,4 +15,16 @@ public class LambdaExpressions {
 
         return tigers;
     }
+
+    public List<Integer> mapExample(){
+        String[] input = {"tiger", "cat", "TIGER", "Tiger", "leopard"};
+        List<String> cats = Arrays.asList(input);
+
+        List<Integer> namesLength = cats.stream()
+                .map(c -> c.length())
+                .collect(Collectors.toList());
+
+        return namesLength;
+    }
+
 }
